65d4446c9988e15f3d414fccfb2276d518e65340
Rathod Arun <arun477rathod@gmail.com>
2026-07-28 15:29:03 +0530

    Changed port for render

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,7 @@
 from app import create_app, socketio
+import os
+port = int(os.environ.get("PORT", 5000))
 
 app = create_app()
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
